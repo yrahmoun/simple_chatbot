62f73d535eb238ce37c5f4e80bf10e9f01d1dd5c
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2025-02-26 18:17:00 +0100

    styled the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import LogInPage from "./pages/LogInPage";
 import Homepage from "./pages/Homepage";
+import "./css/App.css";
 
 function App() {
   return (
-    <div>
+    <>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/Login" element={<LogInPage />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import { useNavigate } from "react-router-dom";
+import "../css/LoginPage.css";
 
 function LogInPage() {
   const [isLogin, setIsLogin] = useState(true);
@@ -14,24 +15,26 @@ function LogInPage() {
   }, [navigate]);
 
   return (
-    <div className="page-conatiner">
-      <div className="login-option">
-        <h1
-          onClick={() => {
-            setIsLogin(true);
-          }}
-        >
-          Login
-        </h1>
-        <h1
-          onClick={() => {
-            setIsLogin(false);
-          }}
-        >
-          register
-        </h1>
+    <div className="page-container">
+      <div className="login-box">
+        <div className="login-option">
+          <h1
+            onClick={() => {
+              setIsLogin(true);
+            }}
+          >
+            Login
+          </h1>
+          <h1
+            onClick={() => {
+              setIsLogin(false);
+            }}
+          >
+            Register
+          </h1>
+        </div>
+        {isLogin ? <Login /> : <Register />}
       </div>
-      {isLogin ? <Login /> : <Register />}
     </div>
   );
 }
